fb185f95ac2ca227d929c66b4461d1661f03a2db
Hamed Naji <hamednajirg@gmail.com>
2020-08-09 14:33:35 +0430

    Initial Commit

---
+++ Covid-19 Dashboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19 Dashboard
+//
+//  Created by Hamed on 5/19/1399 AP.
+//  Copyright © 1399 AP Hamed Naji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
